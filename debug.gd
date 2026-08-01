8aa900b814c8f64e4d47ca92d931efacfb39848b
ramosdetrigo <julia.and.ramos@gmail.com>
2026-08-01 05:14:02 -0300

    Initial load_image testing

--- debug.gd
+++ debug.gd
@@ -1,4 +1,7 @@
-extends Node2D
+extends Control
 
 func _ready() -> void:
-	ImageReader.print()
+	%TRect1.texture = load("res://assets/lagosta.png")
+	
+	var img := ImageReader.load_image(ProjectSettings.globalize_path("res://assets/lagosta.png"))
+	%TRect2.texture = ImageTexture.create_from_image(img)
--- rust/src/image_reader.rs
+++ rust/src/image_reader.rs
@@ -1,15 +1,24 @@
-use godot::prelude::*;
+use godot::{
+    classes::{Image as GodotImage, image::Format},
+    prelude::*,
+};
+use imageproc::image::{self, DynamicImage, RgbImage};
 
 #[derive(GodotClass)]
 #[class(base=Node, init)]
-struct ImageReader {
-
-}
+struct ImageReader {}
 
 #[godot_api]
 impl ImageReader {
     #[func]
-    fn print() {
-        godot_print!("Hello, World!")
+    fn load_image(img_path: String) -> Option<Gd<GodotImage>> {
+        let img = image::open(img_path).ok()?.into_luma8();
+        GodotImage::create_from_data(
+            img.width() as i32,
+            img.height() as i32,
+            false,
+            Format::L8,
+            &img.into_raw().into(),
+        )
     }
 }
